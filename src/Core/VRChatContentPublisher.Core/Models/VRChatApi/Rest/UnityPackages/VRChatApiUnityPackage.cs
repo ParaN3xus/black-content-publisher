5e4f299d58b976c131b866b3b25dd40b21590bbb
paran3xus <paran3xus007@gmail.com>
2026-03-01 11:40:43 +0800

    fix(core): tolerate missing unity package asset fields in world list responses

--- src/Core/VRChatContentPublisher.Core/Models/VRChatApi/Rest/UnityPackages/VRChatApiUnityPackage.cs
+++ src/Core/VRChatContentPublisher.Core/Models/VRChatApi/Rest/UnityPackages/VRChatApiUnityPackage.cs
@@ -4,8 +4,8 @@ namespace VRChatContentPublisher.Core.Models.VRChatApi.Rest.UnityPackages;
 
 public record VRChatApiUnityPackage
 (
-    [property: JsonPropertyName("assetUrl")] string AssetUrl,
-    [property: JsonPropertyName("assetVersion")] int AssetVersion,
-    [property: JsonPropertyName("platform")] string Platform,
-    [property: JsonPropertyName("unityVersion")] string UnityVersion
+    [property: JsonPropertyName("assetUrl")] string AssetUrl = "",
+    [property: JsonPropertyName("assetVersion")] int AssetVersion = 0,
+    [property: JsonPropertyName("platform")] string Platform = "",
+    [property: JsonPropertyName("unityVersion")] string UnityVersion = ""
 );
